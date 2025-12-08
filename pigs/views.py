47d197b89eea248c85496d57fa92d81aca9b0cf0
enzogit <enzo_91_10@users.noreply.github.com>
2025-12-08 17:31:25 -0300

    templates

--- pigs/views.py
+++ pigs/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DetailView, DeleteView
 
 # Create your views here.
+
+#con función
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'pigs/home.html')
+
+#con class based views
 # class Home(TemplateView):
 #     template_name= 'home.html'
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'pigs/about.html')
 
-class ListBooks(ListView):
-    pass
+
